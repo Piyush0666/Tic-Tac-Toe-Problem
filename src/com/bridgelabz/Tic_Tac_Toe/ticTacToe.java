7edbd3f99550c97bdea5902f2a0152a993a7f2bb
your_github_username <your_github_email>
2022-03-09 11:42:39 +0530

    Creating_Tic_Tac_Toe_Board

---
+++ src/com/bridgelabz/Tic_Tac_Toe/ticTacToe.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.Tic_Tac_Toe;
+
+public class ticTacToe {
+    public static char[] gameArray = new char[10];
+
+    public static void ticTacToe() {
+        for (int i = 0; i < gameArray.length; i++) {
+            gameArray[i] = ' ';
+        }
+    }
+
+
+}
